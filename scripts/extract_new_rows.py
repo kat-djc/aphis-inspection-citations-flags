98ba6718d4c4e9d36a869e08a6c675d01b08ed43
kat-djc <kathdjc@gmail.com>
2025-01-03 17:18:56 +0800

    Change of naming conventions for inspections-citations.csv

--- scripts/extract_new_rows.py
+++ scripts/extract_new_rows.py
@@ -23,22 +23,27 @@ def extract_new_rows(original_df, new_df, unique_col=None):
     return new_rows
 
 def main():
+
+    # File paths
+    original_file_path = '../data/output/inspections_citations_latest.csv'
+    new_file_path = '../aphis-inspection-reports/data/combined/inspections-citations.csv'
+
     # Load dataframes
-    df_original = pd.read_csv('../data/last_run_inspections_citations.csv')
-    df_new = pd.read_csv('../aphis-inspection-reports/data/combined/inspections-citations.csv')
+    df_original = pd.read_csv(original_file_path)
+    df_new = pd.read_csv(new_file_path)
 
     # Extract new rows
     new_rows = extract_new_rows(df_original, df_new)
 
     # Save new rows to a CSV file
-    new_rows.to_csv("../data/new_rows/new_inspections_citations.csv", index=False)
+    new_rows.to_csv("../data/flagging_process/new_rows/inspections_citations_new_rows.csv", index=False)
 
     # Print sanity check
     print("----------------------------------------------------------------------------")
     print("Sanity Check for 'extract_new_rows.py':")
     print()
-    print(f"Shape of last run's inspections-citations: {df_original.shape}")
-    print(f"Shape of newest inspections-citations: {df_new.shape}")
+    print(f"Shape of inspections_citations_latest: {df_original.shape}")
+    print(f"Shape of inspections-citations from APHIS: {df_new.shape}")
     print()
     print(f"Shape of extracted new rows: {new_rows.shape}")
     print(f"Shape of expected results: {df_new.shape}")
