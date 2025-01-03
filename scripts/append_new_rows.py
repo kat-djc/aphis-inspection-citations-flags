9a3f6436e88631d6bdbea289e48e9c997bcdf693
kat-djc <kathdjc@gmail.com>
2025-01-03 16:35:55 +0800

    Combined data print statements

--- scripts/append_new_rows.py
+++ scripts/append_new_rows.py
@@ -1,24 +1,31 @@
 import pandas as pd
 
 def main():
-    # Load datasets
-    df_last_run_inspections_citations = pd.read_csv('../data/last_run_inspections_citations.csv')
-    df_inspections_citations_new_rows = pd.read_csv('../data/new_rows/new_inspections_citations.csv')
-    df_inspections_citations = pd.read_csv('../aphis-inspection-reports/data/combined/inspections-citations.csv')
 
-    # Combine datasets
-    df_combined = pd.concat([df_last_run_inspections_citations, df_inspections_citations_new_rows])
+    # File paths
+    original_file_path = '../data/last_run_inspections_citations.csv'
+    new_rows_file_path = '../data/new_rows/new_inspections_citations.csv'
+
+    # Load datasets
+    df_original = pd.read_csv(original_file_path)
+    new_rows = pd.read_csv(new_rows_file_path)
+    df_new = pd.read_csv('../aphis-inspection-reports/data/combined/inspections-citations.csv')
 
     # Print sanity check
     print("----------------------------------------------------------------------------")
-    print("Sanity Check for 'append_flagged_rows.py':")
+    print("Sanity Check for 'append_new_rows.py':")
     print()
-    print(f"Shape of last run inspections-citations data: {df_last_run_inspections_citations.shape}")
-    print(f"Shape of combined dataset: {df_combined.shape}")
-    print(f"Shape of expected dataset (from inspections-citations): {df_inspections_citations.shape}")
-    print()
+    print(f"Shape of last run inspections-citations data: {df_original.shape}")
+    print(f"Shape of expected dataset (from new inspections-citations): {df_new.shape}")
+
+    # Write new rows to last_run_inspections_citations
+    new_rows.to_csv(original_file_path, mode='a', index=False, header=False)
+
+    df_combined = pd.read_csv(original_file_path)
+    print(f"Shape of combined data: {df_combined.shape}")
     print("Ensure the combined dataset matches the expected shape above.")
     print("----------------------------------------------------------------------------")
+
 
 if __name__ == "__main__":
     main()
